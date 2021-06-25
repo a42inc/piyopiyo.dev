bb63fed4a5d185d2895fb85a064ecc1af8b71c3d
Hideyoshi Moriya <hide@piyo.jp>
2021-06-25 09:19:26 +0900

    add scheduled function: runEveryMin

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,3 +4,8 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+exports.runEvery1Min = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+	functions.logger.info("Run every 1 minute", {structuredData: true});
+	return null;
+});
