b19aaa1864e0cc9006e50cd2692a933a940b9351
Hideyoshi Moriya <hide@piyo.jp>
2021-06-25 09:20:02 +0900

    add execRunEvery1Min

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -9,3 +9,12 @@ exports.runEvery1Min = functions.pubsub.schedule('every 1 minutes').onRun((conte
 	functions.logger.info("Run every 1 minute", {structuredData: true});
 	return null;
 });
+
+if (process.env.FUNCTIONS_EMULATOR === 'true') {
+	const { PubSub } = require("@google-cloud/pubsub");
+	const pubsub = new PubSub({  projectId: process.env.GCLOUD_PROJECT });
+	exports.execRunEvery1Min = functions.https.onRequest(async (request, response) => {
+		const msgId = await pubsub.topic('firebase-schedule-runEvery1Min').publishJSON({});
+		response.send(msgId);
+	});
+}
